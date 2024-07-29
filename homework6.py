4f8f16bc4c79b6b37c0f878bafdb4d0ac98c6c62
VitoBJ <Shizane7@gmail.com>
2024-07-29 20:14:19 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,42 @@
+my_dict = {'Vital': 1987, 'Mil':1986, 'Nif Nif': 1980}
+print(my_dict)
+print(my_dict['Nif Nif'])
+my_dict['Nuf Nuf'] =1979
+print(my_dict)
+my_dict.update({'Naf Naf':1978, 'Seriy Volk': 1970})
+print(my_dict)
+a=my_dict.pop('Vital')
+print(my_dict)
+print(a)
+my_set={1,2,3,'что то',3,2,1,('сложно пока и времени мало')}
+print(my_set)
+my_set.discard(2)
+print(my_set)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# hone_book ['Anton'] = 89629594825
+# phone_book.update({'naf naf':232425252353234,
+#                    'nif nif':34647547457,
+#                    'nuf nuf':99987587})
+# print(phone_book)
+# a = phone_book.pop('nif nif')
+# print(phone_book)
+# print(a)
+# list_= [1,2,3]
+# list_.pop(1)
+# print(list_)
+# print(phone_book.items())
